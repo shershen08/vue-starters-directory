55c5f384f294cf3a553d0dab557b8a52da721b30
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-10-23 15:59:31 +0200

    basic example

---
+++ src/components/data.ts
@@ -0,0 +1,42 @@
+import { ListItem } from '@/types/index';
+
+const list:ListItem[] = [
+    {
+        title: 'Vue Starter Kit',
+        published: true,
+        stars: 62,
+        repo: 'https://github.com/ademilter/vue-starter-kit',
+        features: [
+            'router', 'i18n', 'vee-validate', 'axios'
+        ]
+    },
+    {
+        title: 'vue-starter',
+        published: true,
+        stars: 53,
+        repo: 'https://github.com/rohitkrai03/vue-starter',
+        features: [
+            'ESLint', 'i18n', 'vee-validate', 'axios'
+        ]
+    },
+    {
+        title: 'vue-seed',
+        stars: 0,
+        published: true,
+        repo: 'https://github.com/dulin666/vue-seed',
+        features: [
+            'ESLint', 'i18n', 'vee-validate', 'axios'
+        ]
+    },
+    {
+        title: 'vue-express-mongo-boilerplate',
+        stars: 0,
+        published: true,
+        repo: 'https://github.com/icebob/vue-express-mongo-boilerplate',
+        features: [
+            'express', 'mongo', 'backend', 'axios'
+        ]
+    }
+]
+
+export default list;
---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+// declare module '*.vue' {
+// }
+export interface ListItem {
+    published: boolean;
+    title: string;
+    repo: string;
+    stars: number;
+    features: string[];
+};
+
+// export interface List {
+//     ListItem[];
+// }
+// }
+//   export List;
+//   export ListItem;
