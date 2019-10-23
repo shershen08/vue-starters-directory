ec21e562253a90352e4a73203364fe16b9411086
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-10-23 13:02:37 +0200

    sample data

---
+++ public/data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Vue Starter Kit',
+        stars: 62,
+        repo: 'https://github.com/ademilter/vue-starter-kit',
+        features: [
+            'router', 'i18n', 'vee-validate', 'axios'
+        ]
+    },
+    {
+        title: 'vue-starter',
+        stars: 53,
+        repo: 'https://github.com/rohitkrai03/vue-starter',
+        features: [
+            'ESLint', 'i18n', 'vee-validate', 'axios'
+        ]
+    }
+]
