d3b347f7a4a4e37e02cac53abe6a85f9ff49f430
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-10-25 21:47:07 +0200

    build with correct public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     indexPath: '../index.html',
-    publicPath: 'https://shershen08.github.io/vue-starters-directory/dist'
+    publicPath: 'https://shershen08.github.io/vue-starters-directory'
   }
   
